98fa556a759e3a7b1762367e944baa564d05d85f
lexice33 <lexice33@gmail.com>
2018-03-01 00:14:08 +0200

    1

---
+++ Shpora/CodAcademia.py
@@ -0,0 +1,25 @@
+numbers = list(range(3,5))
+print(numbers)
+
+
+
+number = 10
+number += 5
+print(number)
+
+
+# number1 = list(3,4)
+# print(number1)
+
+user = ("Tom", 23)
+print(user)
+
+users = {0: "Tom", 2: "Bob", 3: "Bill"}
+print(users[0])
+
+user_id = 234
+print(type(user_id))
+
+string = "12344556hrk     gkgkwkg"
+string_number = len(string)
+print(string_number)
