404c6a50a988c14a0310ca6c5e72eb9f4f4a9c4a
lexice33 <lexice33@gmail.com>
2018-03-13 23:45:34 +0200

    date_and_time#1

--- Shpora/CodAcademia.py
+++ Shpora/CodAcademia.py
@@ -14,12 +14,72 @@ print(number)
 user = ("Tom", 23)
 print(user)
 
+
+
 users = {0: "Tom", 2: "Bob", 3: "Bill"}
 print(users[0])
 
+
+# '''Выводит тип переменной'''
 user_id = 234
 print(type(user_id))
 
-string = "12344556hrk     gkgkwkg"
+
+
+# '''len Выводит колличество символов в переменной!'''
+string = "12344556hrk        gkgkwkg"
 string_number = len(string)
 print(string_number)
+
+parrot = "Norwegian Blue"
+print(len(parrot))
+
+
+# '''Делает все символы строчные'''
+parrot = "Norwegian 2#3 Blue"
+print(parrot.lower())
+
+
+# '''Все символы заглавные'''
+parrot = "Norwegian Blue"
+print(parrot.upper())
+
+
+# Конкантенация(склеивание) текста """
+print('Spam ' + 'and ' + 'eggs')
+
+
+# Преобразование в строку
+eggs = 2
+print('i ' + 'have ' + str(eggs) + ' eggs')
+
+
+# Объеденение строки с переменной
+eggs = 'Opp'
+name = "Mike"
+print("Hello %s %s" % (name, eggs))
+
+day = 6
+print("03 - %s - 2019" % (day))
+# 03 - 6 - 2019
+print("03 - %02d - 2019" % (day))
+# 03 - 06 - 2019
+
+
+# name = input("What is your name? ")
+# quest = input("What is your quest? ")
+# color = input("What is your favorite color? ")
+#
+# print("Ah, so your name is %s, your quest is %s, " \
+# "and your favorite color is %s." % (name, quest, color))
+
+
+my_string = 'Я буду кодером'
+print(len(my_string))
+print(my_string.upper())
+
+
+
+
+
+
