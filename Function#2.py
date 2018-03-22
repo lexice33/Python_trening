d9ae03cfc0b2efe522fc31a9fa84096a4cc78846
Aleksei <lexice33@gmail.com>
2018-03-22 17:59:06 +0200

    Finction#2

---
+++ Function#2.py
@@ -0,0 +1,33 @@
+def bigger(first, second):
+  print(max(first, second))
+  return True
+
+def answer():
+    return 42
+
+def hotel_cost(nights):
+    return 140 * nights
+
+def plane_ride_cost(city):
+    if city == "Charlotte":
+        return 183
+    elif city == "Tampa":
+        return 220
+    elif city == "Pittsburgh":
+        return 222
+    elif city == "Los Angeles":
+        return 475
+    else:
+        return "Введите валидный город !"
+
+def rental_car_cost(days):
+    car = days * 50
+    if cars >= 7:
+        return car - 50
+    elif car >=3 and car < 7:
+        return car - 20
+    elif 
+print(rental_car_cost(1))
+
+
+
