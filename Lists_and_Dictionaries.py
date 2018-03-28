c3e6a918b26894f9a807c0ca68e6f7191a9258c0
lexice33 <lexice33@gmail.com>
2018-03-29 00:24:04 +0300

    Dictionart#2

--- Lists_and_Dictionaries.py
+++ Lists_and_Dictionaries.py
@@ -75,4 +75,72 @@ print(square_list)
 residents = {'Puffin' : 104, 'Sloth' : 105, 'Burmese Python' : 106}
 
 print(residents['Sloth']) # Prints Puffin's room number
+print('!!!!')
+
+menu = {} # Empty dictionary
+menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
+print(menu['Chicken Alfredo'])
+# Your code here: Add some dish-price pairs to menu!
+menu['Armani'] = 0.5
+menu['sotka'] = 100
+menu['test'] = 'test'
+print("There are " + str(len(menu)) + " items on the menu.")
+print(menu)
+
+razdelitel = '*******************************'
+print(razdelitel)
+
+"""Замена значений пар/ удаление пары словаря"""
+# key - animal_name : value - location
+zoo_animals = { 'Unicorn' : 'Cotton Candy House',
+'Sloth' : 'Rainforest Exhibit',
+'Bengal Tiger' : 'Jungle House',
+'Atlantic Puffin' : 'Arctic Exhibit',
+'Rockhopper Penguin' : 'Arctic Exhibit'}
+# A dictionary (or list) declaration may break across multiple lines
+
+# Removing the 'Unicorn' entry. (Unicorns are incredibly expensive.)
+del zoo_animals['Unicorn']
+
+del zoo_animals['Sloth']
+del zoo_animals['Bengal Tiger']
+
+zoo_animals['Rockhopper Penguin'] = 'Test_zhi_est'
+print(zoo_animals)
+
+print(razdelitel)
+
+"""Удаление элемента из списка"""
+# backpack = ['xylophone', 'dagger', 'tent', 'bread loaf']
+# backpack.remove('dagger')
+# print(backpack)
+#
+# print(razdelitel)
+
+
+inventory = {
+  'gold' : 500,
+  'pouch' : ['flint', 'twine', 'gemstone'], # Assigned a new list to 'pouch' key
+  'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
+}
+
+# Adding a key 'burlap bag' and assigning a list to it
+inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
+
+# Sorting the list found under the key 'pouch'
+inventory['pouch'].sort()
+
+#Добавляем элемент со значениями словаря
+inventory['pocket'] = ['seashell', 'strange berry', 'lint']
+print(inventory)
+#Сортируем значения элемента словаря
+inventory['backpack'].sort()
+
+print(inventory)
+# Удаляем знаение в элементе словаря
+inventory['backpack'].remove('dagger')
+print(inventory)
+# Изменяем значение в элементе словаря
+inventory['gold'] = inventory['gold'] + 50
+print(inventory)
 
